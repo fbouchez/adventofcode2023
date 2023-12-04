b82d5ba2af266a232b96eacaf3470a692022b181
Florent Bouchez Tichadou <florent.bouchez-tichadou@imag.fr>
2023-12-04 16:57:11 +0100

    Initial commit for $execname

---
+++ day04/scratchcards.py
@@ -0,0 +1,11 @@
+import sys
+
+res1 = 0
+res2 = 0
+
+for line in sys.stdin:
+    line = line.strip()
+    print ("Ligne:", line)
+
+print ("Valeur partie 1:", res1)
+print ("Valeur partie 2:", res2)
