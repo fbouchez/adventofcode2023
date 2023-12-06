0d0fff69cea79fd570b6843a8996c6a559e7579f
Florent Bouchez Tichadou <florent.bouchez-tichadou@imag.fr>
2023-12-06 08:15:59 +0100

    day05 partie 2

--- day05/seeds.py
+++ day05/seeds.py
@@ -22,8 +22,16 @@ for line in sys.stdin:
 
     if line.startswith("seeds: "):
         _,s = line.split(": ")
-        seeds = [int(x) for x in s.split()]
+        s = [int(x) for x in s.split()]
+
+        print("longueur:", len(s))
+
+        for i in range(len(s)//2):
+            st = s[2*i]
+            ln = s[2*i+1]
+            seeds.append((st, st+ln-1))
         continue
+
 
     if line.endswith("map:"):
         m,_ = line.split()
@@ -47,43 +55,117 @@ for line in sys.stdin:
 
 print (maps)
 
+smaps = {}
+for m in maps.keys():
+    smaps[m] = sorted(maps[m])
 
 
+print(smaps)
 
-def seedToLoc(x):
+
+print("seeds:", seeds)
+
+sseeds = sorted(seeds)
+
+print("sorted seeds:", sseeds)
+
+
+def seedToLoc(seeds):
+
+    x = seeds
     for i in range(len(order)-1):
         x = applyorder(order[i], order[i+1], x)
+        x = sorted(x)
     return x
 
 
-def applyorder(src, dst, seed):
-    mymap = maps[(src,dst)]
+def applyorder(src, dst, cur_rng_lst):
+    print ('='*80)
+    print (f"applying {src} to {dst} to list {cur_rng_lst}")
 
-    flag = False
+    mymap = smaps[(src,dst)]
 
-    out_seed=seed
-    print('='*80)
-    print(f"Seed en cours de {src} vers {dst}:",seed)
+
+    out_rngs = []
+
+    for (st, end) in cur_rng_lst:
+        out_rngs.extend(transform1range(mymap, st, end))
+
+    return out_rngs
+
+
+
+def transform1range(mymap, st, end):
+
+    out = []
+
+    previous = 1
     for (idx_deb, idx_fin, offset) in mymap:
-        if (idx_deb <= seed) and (seed <= idx_fin):
-            print("range trouvé:", idx_deb, idx_fin, offset)
-            assert not flag
-            out_seed = seed+offset
-            flag = True
 
-    return out_seed
+        new_range, new_st, new_end = apply1range(previous, idx_deb-1, 0, st, end)
+
+        if new_range:
+            out.append(new_range)
+
+        if new_end == None:
+            break
+
+        st = new_st
+        end = new_end
+
+        new_range, new_st, new_end = apply1range(idx_deb, idx_fin, offset, st, end)
+
+        if new_range:
+            out.append(new_range)
+
+        if new_end == None:
+            break
+
+        st = new_st
+        end = new_end
+
+        previous = idx_fin+1
+
+
+    if new_end != None:
+        out.append((st, end))
+
+    return out
 
 
 
 
+def apply1range(idx_deb, idx_fin, offset, st, end):
+
+    if not (idx_deb <= st <= idx_fin):
+        return None, st, end
+
+    if end <= idx_fin:
+        until = end
+        suite = None
+    else:
+        until = idx_fin
+        suite = idx_fin+1
+
+    st_off = st+offset
+    end_off = until+offset
+
+    if not suite:
+        new_st = None
+        new_end = None
+    else:
+        new_st = suite
+        new_end = end
+
+    return (st_off, end_off), new_st, new_end
 
 
 
 print(f"{seeds=}")
-location = list(map(seedToLoc, seeds))
-print(f"{location=}")
+locations = seedToLoc(sseeds)
+print(f"{locations=}")
 
 
-res1 = min(location)
-print ("Valeur partie 1:", res1)
+res2 = min(locations)
+# print ("Valeur partie 1:", res1)
 print ("Valeur partie 2:", res2)
