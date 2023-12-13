d7563b3ec073a284b670b7d7fa2320cf2b34706f
Florent Bouchez Tichadou <florent.bouchez-tichadou@imag.fr>
2023-12-13 11:13:18 +0100

    day12 working part 2

--- day12/hot-springs.hs
+++ day12/hot-springs.hs
@@ -48,11 +48,11 @@ main = do
     print dat
     print $ sum $ map analyseSpring dat
 
-    -- let dat2 = map duplicSpring dat
---
-    -- print $ dat2
---
-    -- print $ sum $ map analyseSpring dat2
+    let dat2 = map duplicSpring dat
+
+    print $ dat2
+
+    print $ sum $ map analyseSpring dat2
 
 
 
@@ -67,25 +67,21 @@ duplicSpring (record, val) = (recx5, valx5)
 analyseSpring (record, val) = do
     let reclen = length record
         vallen = length val
-        valmax = maximum val
         cache = listArray mybounds $ map analyseSpring_idx $ range mybounds
-        mybounds = ((False, 0, 0, 0), (True,reclen, vallen, valmax))
+        mybounds = ((0, 0), (reclen, vallen))
 
 
-        analyseSpring_idx (flag, idx_rec, idx_val, cur_val) = traceShow (("springidx", flag, idx_rec, idx_val, ret)) $ ret
+        analyseSpring_idx (idx_rec, idx_val) = ret
           where
-            ret = aux flag (drop (reclen - idx_rec) record)
-                           values
-            values = 
-              if cur_val == 0 
-                then (drop (vallen - idx_val) val)
-                else (cur_val:(drop (vallen - idx_val+1) val))
-
+            ret = aux drecs dvals
+            drecs = drop (reclen - idx_rec) record
+            dvals = drop (vallen - idx_val) val
 
     -- let aux' flag record val =
-        aux':: Bool -> Int -> Int -> Int -> Int
-        aux' flag idxr idxv curval =
-          traceShow (("array index", flag, idxr, idxv, curval)) $ cache!((flag, idxr, idxv, curval))
+        aux':: Int -> Int -> Int
+        aux' idxr idxv =
+          -- traceShow (("array index", flag, idxr, idxv, curval)) 
+          cache!((idxr, idxv))
 
           -- let res = aux False record val
           -- in
@@ -93,39 +89,40 @@ analyseSpring (record, val) = do
 
 
       -- le booléen indique si on est dans un "intervalle" de #
-        aux False [] [] = traceShow "COUCOU FINI" $ 1
+        aux [] [] = 1
         -- aux False [] [0] = 1
-        aux False [] [0] = 1 -- error "0 should not appear here"
-        aux False [] _ = 0
-        aux False ('.':recs) [] = aux'' False recs []
-        aux False ('?':recs) [] = aux'' False recs []
-        aux False ('#':recs) [] = 0
+        aux [] [0] = 1 -- error "0 should not appear here"
+        aux [] _ = 0
+        aux ('.':recs) [] = aux'' recs []
+        aux ('?':recs) [] = aux'' recs []
+        aux ('#':recs) [] = 0
 
-        aux False (c:recs) (0:vals) =
-            aux'' False (c:recs) (vals)
+        aux (c:recs) (0:vals) =
+            aux'' (c:recs) (vals)
 
-        aux False ('.':recs) (v:vals) = aux'' False recs (v:vals)
-        aux False ('#':recs) (v:vals) =
-            aux'' True recs ((v-1):vals)
+        aux ('.':recs) (v:vals) = aux'' recs (v:vals)
+        aux ('#':recs) (v:vals) =
+            aux_in_range recs ((v-1):vals)
 
-        aux False ('?':recs) (v:vals) =
-            aux'' False recs (v:vals) +  -- on fait comme si ? = .
-            aux'' True recs ((v-1):vals) -- on fait comme si ? = #
-
-
-        aux True _ [] = error "liste des valeurs vides"
-        aux True [] [0] = 1
-        aux True [] _   = 0
-        aux True ('#':recs) (0:_) = 0
-        aux True ('#':recs) (v:vals) = aux'' True recs ((v-1):vals)
-        aux True ('.':recs) (0:vals) = aux'' False recs vals
-        aux True ('.':recs) (v:vals) = 0
-        aux True ('?':recs) (0:vals) = aux'' False recs vals -- ? = .
-        aux True ('?':recs) (v:vals) = aux'' True recs ((v-1):vals) -- ?= #
+        aux ('?':recs) (v:vals) =
+            aux'' recs (v:vals) +  -- on fait comme si ? = .
+            aux_in_range recs ((v-1):vals) -- on fait comme si ? = #
 
 
-        aux'' flag rec [] = aux' flag (length rec) 0 0
-        aux'' flag rec (curv:vals) = aux' flag (length rec) (length (curv:vals)) curv
+        aux_in_range _ [] = error "liste des valeurs vides"
+        aux_in_range [] [0] = 1
+        aux_in_range [] _   = 0
+        aux_in_range ('#':recs) (0:_) = 0
+        aux_in_range ('#':recs) (v:vals) = aux_in_range recs ((v-1):vals)
+        aux_in_range ('.':recs) (0:vals) = aux'' recs vals
+        aux_in_range ('.':recs) (v:vals) = 0
+        aux_in_range ('?':recs) (0:vals) = aux'' recs vals -- ? = .
+        aux_in_range ('?':recs) (v:vals) = aux_in_range recs ((v-1):vals) -- ?= #
+
+
+        -- aux'' rec [] = aux' (length rec) 0 0
+        aux'' recs vals = aux' (length recs) (length vals)
 
     -- traceShow (cache!(False,0,0)) $
-    aux' False reclen vallen (head val)
+    -- aux' False reclen vallen (head val)
+    cache!(reclen, vallen)
